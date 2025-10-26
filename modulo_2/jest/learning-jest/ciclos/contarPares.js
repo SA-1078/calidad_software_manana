6776adc7d1217673c721a59a886f5c1b6db75d8d
unknown <santiagocolimba10@gmail.com>
2025-10-26 17:12:26 -0500

    Clase Viernes 24 de octubre 2025 calidad de software

---
+++ modulo_2/jest/learning-jest/ciclos/contarPares.js
@@ -0,0 +1,16 @@
+// ESTE ARCHIVO VA EN EL REPOSITORIO DE CALIDAD DE SOFTWARE DENTRO DE LA CARPETA LEARNING-JEST
+//  Y DENTRO DE LA SUBCARPETA CICLOS
+
+function contarPares(arreglo) {
+    if (!Array.isArray(arreglo)
+        ) {
+        throw new TypeError("arreglo invalido");
+        }
+    let contador = 0;
+    for (let item of arreglo) {
+        if(x%2===0) contador++;
+    }
+    return contador;
+}
+
+module.exports = { contarPares };
